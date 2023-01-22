f0c329da26afe72cbb410d80b98caf212c5e048c
sunkumar12 <sanjeetkumark83@gmail.com>
2023-01-22 15:44:10 +0530

    first commit

---
+++ 3.py
@@ -0,0 +1,25 @@
+import turtle as n
+import random
+n.colormode(255)
+n.speed(10)
+n.pensize(4)
+for i in range(12):
+    n.color('green')
+    n.forward(100)
+    n.left(60)
+    n.forward(20)
+    n.left(60)
+    n.forward(50)
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    n.color(r,g,b)
+    for i in range(10):
+        n.forward(30)
+	n.left(160)
+    n.right(30)
+    n.penup()
+    n.setposition(0, 0)
+    n.pendown()
+    n.right(10)
+turtle.done()
---
+++ 45.py
@@ -0,0 +1,24 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+import random
+import time
+delay=0.1
+score=0
+heighestscore=0
+bodies=[]
+s=turtle.screen()
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    s=turtle.screen()
+AttributeError: module 'turtle' has no attribute 'screen'. Did you mean: 'Screen'?
+S=turtle.screen()
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    S=turtle.screen()
+AttributeError: module 'turtle' has no attribute 'screen'. Did you mean: 'Screen'?
+import canvas
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    import canvas
+ModuleNotFoundError: No module named 'canvas'
